bebea30a04f153b85e094fad496f9009f043c612
liuwanlin <liuwanlin@jollytech.com.cn>
2017-05-09 18:42:09 +0800

    add themes

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
+var plumber = require('gulp-plumber');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
 var preserveFirstComment = function() {
   var set = false;
@@ -12,6 +15,14 @@ var preserveFirstComment = function() {
   };
 };
 
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
 gulp.task('uglify', function() {
   gulp.src('lib/marked.js')
     .pipe(uglify({preserveComments: preserveFirstComment()}))
@@ -19,4 +30,21 @@ gulp.task('uglify', function() {
     .pipe(gulp.dest('.'));
 });
 
-gulp.task('default', ['uglify']);
+gulp.task('sass', function () {
+  return gulp.src('./themes/**/*.scss')
+    .pipe(plumber())
+    .pipe(sass({
+        outputStyle: 'expanded'
+    }))
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./themes/**/*.scss', ['sass']);
+
+  gulp.watch('./themes/**/*.scss').on('change', browserSync.reload);
+  gulp.watch('./test/t/**/*').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['uglify', 'sass', 'watch', 'browser-sync']);
